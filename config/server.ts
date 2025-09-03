8947d758b6af09098fea34b58dd1d834251df829
bashar8606 <ahammed.bashar9@gmail.com>
2025-09-03 22:33:50 +0400

    late

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '3.91.65.86'),
+  host: env('HOST', '40.172.190.110'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://3.91.65.86:1337'),
+  url: env('PUBLIC_URL', 'http://40.172.190.110:1337'),
   proxy: true, // <— important for Render / Heroku
   app: {
     keys: env.array('APP_KEYS', ['key1', 'key2']),
