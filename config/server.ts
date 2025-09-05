56723953b6fe3a976a113f2d2ebad9fdffc77ed4
bashar8606 <ahammed.bashar9@gmail.com>
2025-09-06 00:52:18 +0400

    latest

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://40.172.190.110:1337'),
+  url: env('PUBLIC_URL', 'https://admin.calladoc.ae'),
   proxy: true, // <— important for Render / Heroku
   app: {
     keys: env.array('APP_KEYS', ['key1', 'key2']),
